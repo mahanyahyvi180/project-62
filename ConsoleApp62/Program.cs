91481822bed66a9c6641bdb339d563bac4bf2d3f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 10:17:14 +0330

    Add files via upload

---
+++ ConsoleApp62/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shoa = 5;
+            const float pi = 3.14f;
+            float Mas;
+            float Moh;
+
+            Mas = pi * shoa * shoa;
+            Moh = 2 * pi * shoa;
+
+            Console.WriteLine("Mohit" + Moh);
+            Console.WriteLine("Masahat" + Mas);
+
+            Console.ReadLine();
+        }
+    }
+}
